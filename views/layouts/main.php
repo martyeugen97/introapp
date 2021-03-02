dc30acb2dd7dc6b540236d76c3870162fac6b015
martyeugen97 <martyeugen97@yandex.ru>
2021-03-02 16:48:49 +0300

    Order page mock view

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -26,52 +26,33 @@ AppAsset::register($this);
 <body>
 <?php $this->beginBody() ?>
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
-    ]);
-    NavBar::end();
-    ?>
+<nav class="navbar navbar-fixed-top navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Orders</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
+<div class="wrap">
     <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
         <?= $content ?>
     </div>
 </div>
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-left">&copy; Marty Eugen <?= date('Y') ?></p>
     </div>
 </footer>
 
--- views/site/index.php
+++ views/site/index.php
@@ -2,52 +2,156 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Yii app';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
     <div class="body-content">
 
+        <h2>Heading</h2>
+
+        <ul class="nav nav-tabs p-b">
+            <li class="active"><a href="#">All orders</a></li>
+            <li><a href="#">Pending</a></li>
+            <li><a href="#">In progress</a></li>
+            <li><a href="#">Completed</a></li>
+            <li><a href="#">Canceled</a></li>
+            <li><a href="#">Error</a></li>
+            <li class="pull-right custom-search">
+                <form class="form-inline" action="/admin/orders" method="get">
+                    <div class="input-group">
+                        <input type="text" name="search" class="form-control" value="" placeholder="Search orders">
+                        <span class="input-group-btn search-select-wrap">
+
+            <select class="form-control search-select" name="search-type">
+              <option value="1" selected="">Order ID</option>
+              <option value="2">Link</option>
+              <option value="3">Username</option>
+            </select>
+            <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+            </span>
+                    </div>
+                </form>
+            </li>
+        </ul>
+
+        <table class="table order-table">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>User</th>
+                <th>Link</th>
+                <th>Quantity</th>
+                <th class="dropdown-th">
+                    <div class="dropdown">
+                        <button class="btn btn-th btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                            Service
+                            <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                            <li class="active"><a href="">All (894931)</a></li>
+                            <li><a href=""><span class="label-id">214</span>  Real Views</a></li>
+                            <li><a href=""><span class="label-id">215</span> Page Likes</a></li>
+                            <li><a href=""><span class="label-id">10</span> Page Likes</a></li>
+                            <li><a href=""><span class="label-id">217</span> Page Likes</a></li>
+                            <li><a href=""><span class="label-id">221</span> Followers</a></li>
+                            <li><a href=""><span class="label-id">224</span> Groups Join</a></li>
+                            <li><a href=""><span class="label-id">230</span> Website Likes</a></li>
+                        </ul>
+                    </div>
+                </th>
+                <th>Status</th>
+                <th class="dropdown-th">
+                    <div class="dropdown">
+                        <button class="btn btn-th btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                            Mode
+                            <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                            <li class="active"><a href="">All</a></li>
+                            <li><a href="">Manual</a></li>
+                            <li><a href="">Auto</a></li>
+                        </ul>
+                    </div>
+                </th>
+                <th>Created</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>558931</td>
+                <td>waliullah</td>
+                <td class="link">/p/BMRSv4FDevy/</td>
+                <td>3000</td>
+                <td class="service">
+                    <span class="label-id">213</span>Likes
+                </td>
+                <td>Pending</td>
+                <td>Manual</td>
+                <td><span class="nowrap">2016-01-27</span><span class="nowrap">15:13:52</span></td>
+            </tr>
+            <tr>
+                <td>55892</td>
+                <td>spiderfady</td>
+                <td class="link">/followers</td>
+                <td>1800</td>
+                <td class="service">
+                    <span class="label-id">3</span> Real Views
+                </td>
+                <td>Canceled</td>
+                <td>Auto</td>
+                <td><span class="nowrap">2016-01-27</span><span class="nowrap">15:13:52</span></td>
+            </tr>
+            <tr>
+                <td>55891</td>
+                <td>spiderfady</td>
+                <td class="link">/com.usk</td>
+                <td>1800</td>
+                <td class="service">
+                    <span class="label-id">15</span> Views
+                </td>
+                <td>Canceled</td>
+                <td>Auto</td>
+                <td><span class="nowrap">2016-01-27</span><span class="nowrap">15:13:52</span></td>
+            </tr>
+            <tr>
+                <td>52137</td>
+                <td>gulaka</td>
+                <td class="link">/p/BMD5RzxgRke/</td>
+                <td>1800</td>
+                <td class="service">
+                    <span class="label-id">5</span> Comment
+                </td>
+                <td>Error</td>
+                <td>Auto</td>
+                <td><span class="nowrap">2016-01-27</span><span class="nowrap">15:13:52</span></td>
+            </tr>
+            </tbody>
+        </table>
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="col-sm-8">
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <nav>
+                    <ul class="pagination">
+                        <li class="disabled"><a href="" aria-label="Previous">&laquo;</a></li>
+                        <li class="active"><a href="">1</a></li>
+                        <li><a href="">2</a></li>
+                        <li><a href="">3</a></li>
+                        <li><a href="">4</a></li>
+                        <li><a href="">5</a></li>
+                        <li><a href="">6</a></li>
+                        <li><a href="">7</a></li>
+                        <li><a href="">8</a></li>
+                        <li><a href="">9</a></li>
+                        <li><a href="">10</a></li>
+                        <li><a href="" aria-label="Next">&raquo;</a></li>
+                    </ul>
+                </nav>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            <div class="col-sm-4 pagination-counters">
+                1 to 100 of 3263
             </div>
         </div>
-
     </div>
 </div>
